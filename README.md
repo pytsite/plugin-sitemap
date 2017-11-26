93a1779bee8c3eec422fcef651f1464c32eb74e0
Alexander Shepetko <a@shepetko.com>
2017-11-26 13:31:00 +0200

    Release 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# PytSite Sitemap Plugin
+
+
+## Changelog
+
+
+### 1.0 (2017-11-26)
+
+First release.
---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""PytSite Sitemap Plugin.
+"""
+# Public API
+from ._sitemap import Sitemap, Index
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
